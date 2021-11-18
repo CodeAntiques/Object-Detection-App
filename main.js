e345d9020c91d2127bda4595b959480b6d79a8af
Hanish565 <74765868+Hanish565@users.noreply.github.com>
2021-11-18 17:34:27 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,47 @@
+img=""
+status=""
+
+
+
+function preload(){
+  img=loadImage("dog_cat.jpg");
+}
+
+function draw(){
+   image(img,0,0,640,460);
+   fill(red,green,blue);
+   text("Dog",120,80)
+   noFill();
+   stroke(red,green,blue);
+   rect(110,60,200,390)
+
+   fill(red,green,blue);
+   text("Cat",339,80)
+   noFill();
+   stroke(red,green,blue);
+   rect(320,60,200,360)
+}
+
+function setup(){
+    canvas=createCanvas(640,460);
+    canvas.center();
+    objectDetector=ml5.objectDetector("cocossd", modeloaded);
+    document.getElementById("status").innerHTML="Status:Detecting Object";
+    red=Math.floor(Math.random()*256);
+    green=Math.floor(Math.random()*256);
+    blue=Math.floor(Math.random()*256);
+}
+
+function modeloaded(){
+  console.log("Model has been loaded")
+  status=true;
+  objectDetector.detect(img,gotResults);
+}
+
+function gotResults(results,error){
+  if (error) {
+    console.error(error);
+  } else {
+    console.log(results);
+  }
+}
