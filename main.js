fc9e52b5b39a81f50e68b1feaa9d9b5b7a768850
Hanish565 <74765868+Hanish565@users.noreply.github.com>
2021-11-22 18:03:53 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 img=""
 status=""
-
-
+objects=[];
+percentage=0;
 
 function preload(){
   img=loadImage("dog_cat.jpg");
@@ -9,17 +9,19 @@ function preload(){
 
 function draw(){
    image(img,0,0,640,460);
-   fill(red,green,blue);
-   text("Dog",120,80)
-   noFill();
-   stroke(red,green,blue);
-   rect(110,60,200,390)
-
-   fill(red,green,blue);
-   text("Cat",339,80)
-   noFill();
-   stroke(red,green,blue);
-   rect(320,60,200,360)
+if(status !=""){
+  console.log(objects);
+   for ( i = 0; i < objects.length; i++) {
+      document.getElementById("status").innerHTML="Status:Object Detected";
+      fill(red,green,blue);
+      percentage=floor(objects[i].confidence*100)
+      text(objects[i].label + " " + percentage + " % ",objects[i].x+30,objects[i].y+30);
+      noFill();
+      stroke(red,green,blue);
+      rect(objects[i].x,objects[i].y,objects[i].width,objects[i].height);
+      console.log(objects.length);
+   }
+  }
 }
 
 function setup(){
@@ -38,10 +40,11 @@ function modeloaded(){
   objectDetector.detect(img,gotResults);
 }
 
-function gotResults(results,error){
+function gotResults(error,results){
   if (error) {
     console.error(error);
-  } else {
+  }  
     console.log(results);
-  }
+    objects=results;
+  
 }
